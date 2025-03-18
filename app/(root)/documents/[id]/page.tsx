db654dae72f18373d0a0e55b63bde97ff7b37286
Nehardh Madapuri <136841922+nehardh@users.noreply.github.com>
2025-03-18 13:59:46 +0530

    progress-2

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,15 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Document = () => {
+  return (
+      <main className="flex w-full flex-col items-center">
+        <CollaborativeRoom/>
+      </main>
+  )
+}
+
+export default Document
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Provider from "./Provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Live Docs",
+  description: "Real-time Doc collaboration",
+};
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
